ca8c8644ef290477c13483d3b04e262d0176f397
Carlos Abraao <carloschavesnd@hotmail.com>
2021-12-10 10:16:25 -0300

    criado projeto consumo cep
    Coloquei alguns import a mais

---
+++ consumerAPiCepGson/src/main/java/com/devcarlos/apicep/ConsumerAPiCepGsonApplication.java
@@ -0,0 +1,13 @@
+package com.devcarlos.apicep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerAPiCepGsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerAPiCepGsonApplication.class, args);
+	}
+
+}
---
+++ consumerAPiCepGson/src/test/java/com/devcarlos/apicep/ConsumerAPiCepGsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcarlos.apicep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerAPiCepGsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
