9dd359d030050fed26b6d5ecde2e9f582e47290f
Carlos Abraao <carloschavesnd@hotmail.com>
2021-12-13 09:10:50 -0300

    consumo da API_CEP (Jersey)

--- consumerAPiCepGson/src/main/java/com/devcarlos/apicep/ConsumerAPiCepGsonApplication.java
+++ consumerAPiCepGson/src/main/java/com/devcarlos/apicep/ConsumerAPiCepGsonApplication.java
@@ -3,11 +3,19 @@ package com.devcarlos.apicep;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devcarlos.apicep.controller.ControladorCepBean;
+
+
 @SpringBootApplication
 public class ConsumerAPiCepGsonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerAPiCepGsonApplication.class, args);
+		
+		ControladorCepBean bean = new ControladorCepBean();
+		
+		System.out.println(bean.carregarEndereco());
+		
 	}
 
 }
